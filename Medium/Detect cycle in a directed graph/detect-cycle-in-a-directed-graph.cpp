fba5f57c247185b53ff8fc38662fcc6f19bf9bef
Ayush Jha <73348925+ayushjha119@users.noreply.github.com>
2023-12-08 09:51:46 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Detect cycle in a directed graph/detect-cycle-in-a-directed-graph.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    bool dfs(int node, vector<int> &vis, vector<int> adj[]){
+        vis[node]=1;
+        
+        for(auto child: adj[node]){
+            if(vis[child]==0){ //child node not visited yet
+                if(dfs(child,vis,adj)){
+                    return true; //returning true if child node return presence of cycle
+                }
+            }
+            
+            else if(vis[child]==1){ //node is still under processing and appeared again
+                return true; //then there is back edge, finally cycle
+            }
+        }
+        
+        vis[node]=2; //marks evaluation is complete
+        return false;
+    }
+    
+    
+    bool isCyclic(int v, vector<int> adj[]) {
+        
+        
+        vector<int> vis(v,0);
+        for(int i=0;i<v;i++){
+            if(vis[i]==0){
+                if(dfs(i,vis,adj)){
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int V, E;
+        cin >> V >> E;
+
+        vector<int> adj[V];
+
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+        }
+
+        Solution obj;
+        cout << obj.isCyclic(V, adj) << "\n";
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
