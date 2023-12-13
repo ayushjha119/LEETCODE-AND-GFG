5f9e2c6b6acb330a5e0b765aa81481a3a867d020
Ayush Jha <73348925+ayushjha119@users.noreply.github.com>
2023-12-13 12:51:35 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Max length chain/max-length-chain.cpp
@@ -0,0 +1,55 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct val{
+	int first;
+	int second;
+};
+
+
+// } Driver Code Ends
+/*
+The structure to use is as follows
+struct val{
+	int first;
+	int second;
+};*/
+
+class Solution{
+public:
+    /*You are required to complete this method*/
+    int maxChainLen(struct val p[],int n){
+        //Your code here
+        
+        sort(p,p+n, [](const val& a, const val& b){    return a.second < b.second; });
+        
+        int ans = 0, maxi = 0;
+        
+         for(int i{0} ; i<n; i++) if(maxi < p[i].first)   ans++ , maxi = p[i].second;
+
+     return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		val p[n];
+		for(int i=0;i<n;i++)
+		{
+			cin>>p[i].first>>p[i].second;
+		}
+		Solution ob;
+		cout<<ob.maxChainLen(p,n)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
