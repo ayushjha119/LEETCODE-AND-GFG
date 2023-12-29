e82848b3ac83db2065247352bc978762887468af
Ayush Jha <73348925+ayushjha119@users.noreply.github.com>
2023-12-29 11:40:44 +0530

    Updated Solution - GfG to GitHub

--- Medium/Maximum Index/maximum-index.cpp
+++ Medium/Maximum Index/maximum-index.cpp
@@ -1,51 +1,57 @@
 //{ Driver Code Starts
-#include <bits/stdc++.h>
-
+#include<bits/stdc++.h>
 using namespace std;
+
 
 // } Driver Code Ends
 class Solution{
-public:
-    int maxIndexDiff(int arr[], int n) {
-        // code here
-        int i,j,ans=0;
-        vector<int>mn(n),mx(n);
-        mn[0]=arr[0];
-        mx[n-1]=arr[n-1];
-        for(i=1;i<n;i++){
-            mn[i]=min(mn[i-1],arr[i]);
-            mx[n-i-1]=max(mx[n-i],arr[n-i-1]);
+    public:
+        
+    // A[]: input array
+    // N: size of array
+    // Function to find the maximum index difference.
+    int maxIndexDiff(int arr[], int n) 
+    { 
+        // Your code here
+          vector<int> prefixMin(n);
+        prefixMin[0]=arr[0];
+        for(int i=1;i<n;i++) {
+            prefixMin[i]=min(prefixMin[i-1],arr[i]);
         }
-        i=0,j=0;
-        while(i<n && j<n){
-            while(j<n && mn[i]<=mx[j]){
-                j++;
-            }
-            ans=max(ans,j-i-1);
-            i++;
+        int i=n-1,j=n-1,maxlen=INT_MIN;
+        while(i>=0 and j>=0) {
+            if(prefixMin[i]<=arr[j]) {
+                maxlen=max(maxlen,j-i);
+                i--;
+            } else j--;
         }
-        return ans;
+        return maxlen;
     }
-
 };
 
-
 //{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n, i;
-        cin >> n;
-        int a[n];
-        for (i = 0; i < n; i++) {
-            cin >> a[i];
-        }
+  
+/* Driver program to test above functions */
+int main() 
+{
+    int T;
+    //testcases
+    cin>>T;
+    while(T--){
+        int num;
+        //size of array
+        cin>>num;
+        int arr[num];
+        
+        //inserting elements
+        for (int i = 0; i<num; i++)
+            cin>>arr[i];
         Solution ob;
-        auto ans = ob.maxIndexDiff(a, n);
-        cout << ans << "\n";
+        
+        //calling maxIndexDiff() function
+        cout<<ob.maxIndexDiff(arr, num)<<endl;    
+        
     }
     return 0;
-}
+} 
 // } Driver Code Ends
