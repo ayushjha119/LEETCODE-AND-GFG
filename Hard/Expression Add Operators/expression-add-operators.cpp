e96617a5374197bfca72e276bda631e3fddd23cd
Ayush Jha <73348925+ayushjha119@users.noreply.github.com>
2024-01-10 12:02:56 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Expression Add Operators/expression-add-operators.cpp
@@ -0,0 +1,61 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<string> ans;
+    string s;
+    int target;
+    vector<string> addOperators(string S, int target) {
+        // code here
+        this->target = target;
+        this->s = S;
+        gen_exp(0, 0, 0, "");
+        return ans;
+    }
+    void gen_exp(int ind, long prev, long val, string ex){
+        if(ind == s.size()){
+            if(val == target)   ans.push_back(ex);
+            return;
+        }
+        int n = s.size();
+        string ns = "";
+        int curr = 0;
+        for(int i=ind;i<n;i++){
+            if(i>ind and s[ind] == '0') return;
+            ns += s[i];
+            curr = curr*10 + s[i] - '0';
+            if(ind == 0){
+                gen_exp(i+1, curr, curr, ex+ns);
+                continue;
+            }
+            
+            gen_exp(i+1,curr,val+curr,ex + "+" + ns);
+            gen_exp(i+1,-curr,val-curr,ex + "-" + ns);
+            gen_exp(i+1,prev*curr,val-prev+prev*curr,ex + "*" + ns);
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int target;
+        string S;
+        cin >> S >> target;
+
+        Solution obj;
+        vector<string> ans = obj.addOperators(S, target);
+        sort(ans.begin(), ans.end());
+        for (auto &it : ans) cout << it << ' ';
+        cout << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
